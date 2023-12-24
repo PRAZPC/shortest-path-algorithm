7c1985fca91818eff318f2965389a1523e47d6fa
Prazwal Chapagain <65542395+PRAZPC@users.noreply.github.com>
2023-12-24 18:04:00 +0000

    change

--- function.py
+++ function.py
@@ -1,4 +1,23 @@
-from pyamaze import maze
-my =maze(5,5)
-my.CreateMaze()
-my.run()
+import sys 
+from heapq import heapify ,heappush , heappop
+
+def algo(graph , start,end):
+    inf = sys.maxsize
+    node={'A':{'Current':inf,'Next':[]}, {'B':{'Current':inf,'Next':[]}} , {'C':{'Current':inf,'Next':[]}},{'D':{'Current':inf,'Next':[]}}
+    }
+
+
+if __name__="__main__":
+    graph=
+    {
+        'A':{'B':2,'C':4},
+        'B':{'A':2,'C':3,'D':2},
+        'C':{'A':4,'B':3,'E':5,'D':2},
+        'D':{'B':8,'C':2}
+
+    }
+
+start = 'A'
+end = []
+
+
--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-for function import *
+from function import algo
+from heapq import heapify ,heappush , heappop
+
+
+
+if __name__ == "__main__":
+    graph = {
+        'A':{'B':2,'C':4},
+        'B':{'A':2,'C':3,'D':2},
+        'C':{'A':4,'B':3,'E':5,'D':2},
+        'D':{'B':8,'C':2}
+
+    }
+    
 
