dc069f6e1df61fe40a42c6d10ce8327e26cf5442
Prazwal Chapagain <65542395+PRAZPC@users.noreply.github.com>
2023-12-25 12:08:31 +0000

    error fix

--- function.py
+++ function.py
@@ -3,15 +3,24 @@ from heapq import heapify ,heappush , heappop
 
 def algo(graph , start,end):
     inf = sys.maxsize
-    node={'A':{'Current':inf,'Next':[]}, {'B':{'Current':inf,'Next':[]}} , {'C':{'Current':inf,'Next':[]}},{'D':{'Current':inf,'Next':[]}}
+    node={{'A':{'Current':inf,'Next':[]}}, {'B':{'Current':inf,'Next':[]} }, {'C':{'Current':inf,'Next':[]}},{'D':{'Current':inf,'Next':[]}}
     }
+    node[start]['Current']=0
+    visited = []
+    temp = start 
+    for i in range(5):
+        if temp not in visited :
+            visited.append(temp)
+            min =[]
+            for j in graph[temp]:
+                if j not in visited :
+                    Current = node[temp]['Current'] + graph[temp][j]
 
-    
 
 
-if __name__="__main__":
-    graph=
-    {
+
+if __name__=="__main__":
+    graph = {
         'A':{'B':2,'C':4},
         'B':{'A':2,'C':3,'D':8},
         'C':{'A':4,'B':3,'E':5,'D':2},
