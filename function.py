5e078993ba7b40ea2f32bb0fa2bb49b70bcbc9f0
Prazwal Chapagain <65542395+PRAZPC@users.noreply.github.com>
2023-12-25 12:36:01 +0000

    update

--- function.py
+++ function.py
@@ -30,14 +30,6 @@ def algo(graph, start, end):
     print('short distance is ', str(node[end]['Current']))
     print('shortest path is ', str(node[end]['Next'] + [end]))
 
-if __name__ == "__main__":
-    graph = {
-        'A': {'B': 2, 'C': 4},
-        'B': {'A': 2, 'C': 3, 'D': 8},
-        'C': {'A': 4, 'B': 3, 'E': 5, 'D': 2},
-        'D': {'B': 8, 'C': 2, 'E': 11, 'F': 22},
-        'E': {'C': 5, 'D': 11, 'F': 1},
-        'F': {'D': 22, 'E': 1}
-    }
+
 
    
