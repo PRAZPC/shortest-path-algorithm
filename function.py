4c38734bba5bd21f4edbaaf872f288ff811ddce9
Prazwal Chapagain <65542395+PRAZPC@users.noreply.github.com>
2023-12-25 11:59:10 +0000

    update graph

--- function.py
+++ function.py
@@ -6,18 +6,23 @@ def algo(graph , start,end):
     node={'A':{'Current':inf,'Next':[]}, {'B':{'Current':inf,'Next':[]}} , {'C':{'Current':inf,'Next':[]}},{'D':{'Current':inf,'Next':[]}}
     }
 
+    
+
 
 if __name__="__main__":
     graph=
     {
         'A':{'B':2,'C':4},
-        'B':{'A':2,'C':3,'D':2},
+        'B':{'A':2,'C':3,'D':8},
         'C':{'A':4,'B':3,'E':5,'D':2},
-        'D':{'B':8,'C':2}
+        'D':{'B':8,'C':2,'E':11,'F':22} ,
+        'E':{'C':5,'D':11 ,'F':1},
+        'F': {'D':22,'E':1}
 
     }
 
 start = 'A'
-end = []
+end = 'F'
+
 
 
